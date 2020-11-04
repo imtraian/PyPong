d2b0f9509aacf0d29c9c4d08d1407c59b0bc1328
imtraian <imtraian@outlook.com>
2020-11-04 17:14:21 +0200

    :video_game:: Add sound
    Finally
    
    Signed-off-by: imtraian <imtraian@outlook.com>

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import turtle
+import winsound
 
 wn = turtle.Screen()
 wn.title("Pong by @imtraian")
@@ -83,10 +84,12 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        winsound.PlaySound("bounce", winsound.SND_ASYNC)
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        winsound.PlaySound("bounce", winsound.SND_ASYNC)
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
@@ -107,7 +110,9 @@ while True:
     if ball.xcor() > 340 and ball.xcor() < 350 and ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40:
         ball.setx(340)
         ball.dx *= -1
+        winsound.PlaySound("bounce", winsound.SND_ASYNC)
 
     if ball.xcor() < -340 and ball.xcor() > -350 and ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40:
         ball.setx(-340)
         ball.dx *= -1
+        winsound.PlaySound("bounce", winsound.SND_ASYNC)
