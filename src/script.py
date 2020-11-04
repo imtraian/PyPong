c377c382250c3d6cbf4a56f3a1f10fad46f37d1c
imtraian <imtraian@outlook.com>
2020-11-04 15:28:05 +0200

    :video_game:: Implement ball movement
    With board checking. This was a pain in ass
    
    Signed-off-by: imtraian <imtraian@outlook.com>

--- src/script.py
+++ src/script.py
@@ -28,6 +28,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.1
+ball.dy = 0.1
 
 
 def paddle_a_up():
@@ -62,3 +64,22 @@ wn.onkeypress(paddle_b_down, "Down")
 
 while True:
     wn.update()
+
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
